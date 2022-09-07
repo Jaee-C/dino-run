4d9fbafcb14d3d307f07cf02bc902f62c90f5901
thebowenfeng <857514.leofeng@gmail.com>
2022-09-07 18:06:18 +1000

    refactored code, added proper obstacle collision

--- Assets/GeneratePlane.cs
+++ Assets/GeneratePlane.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class GeneratePlane : MonoBehaviour
 {
-    static GameObject savedPlane;
-    public static GameObject plane;
-    static GameObject obstacle;
-    private static Vector3 endPosition;
-    private static Vector3 offset;
+    private GameObject savedPlane;
+    private GameObject plane;
+    private GameObject obstacle;
+    private Vector3 endPosition;
+    private Vector3 offset;
 
-    static void generateObstacles()
+    public void generateObstacles()
     {
-        int numGen = 0;
         for (float x = plane.transform.position.x - 5; x < plane.transform.position.x + 5; x++)
         {
             for(float z = plane.transform.position.z - 5; z < plane.transform.position.z + 5; z++)
@@ -20,7 +19,6 @@ public class GeneratePlane : MonoBehaviour
                 float height = Random.value * 10f;
                 if(height >= 9.8f)
                 {
-                    numGen++;
                     GameObject generatedObstacle = Instantiate(obstacle, new Vector3(0, 0, 0), Quaternion.identity);
                     generatedObstacle.transform.parent = plane.transform;
                     generatedObstacle.transform.localScale = new Vector3(1, height - 7f, 1);
@@ -28,10 +26,9 @@ public class GeneratePlane : MonoBehaviour
                 }
             }
         }
-        Debug.Log(numGen);
     }
 
-    public static void spawnPlane()
+    public void spawnPlane()
     {
         GameObject temp = Instantiate(savedPlane, endPosition, Quaternion.identity);
         endPosition = plane.transform.GetChild(0).transform.position + offset;
@@ -42,7 +39,7 @@ public class GeneratePlane : MonoBehaviour
     void Start()
     {
         savedPlane = GameObject.Find("Plane");
-        plane = Instantiate(savedPlane, savedPlane.transform.position, Quaternion.identity);
+        plane = Instantiate(savedPlane, new Vector3(0, 0, 0), Quaternion.identity);
         offset = savedPlane.transform.GetChild(0).transform.position - savedPlane.transform.position;
 
         obstacle = GameObject.Find("Obstacle");
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,25 +4,75 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Rigidbody rigidBody;
+    private GeneratePlane planeGenerator;
+    private bool isColliding = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        planeGenerator = FindObjectOfType<GeneratePlane>();
+        rigidBody.velocity = new Vector3(0, 0, 10f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position += new Vector3(0, 0, 0.1f);
+        //rigidBody.MovePosition(this.transform.position + new Vector3(0, 0, 0.2f));
+        //rigidBody.AddForce(new Vector3(0, 0, .2f), mode: ForceMode.VelocityChange
+
+        bool isMoving = false;
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            this.transform.position += new Vector3(-0.03f, 0, 0);
+            if (isColliding)
+            {
+                rigidBody.velocity = new Vector3(-10f, 0, 0);
+            }
+            else
+            {
+                rigidBody.velocity = new Vector3(-10f, 0, 10f);
+            }
+            isMoving = true;
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            this.transform.position += new Vector3(0.03f, 0, 0);
+            if (isColliding)
+            {
+                rigidBody.velocity = new Vector3(10f, 0, 0);
+            }
+            else
+            {
+                rigidBody.velocity = new Vector3(10f, 0, 10f);
+            }
+            isMoving = true;
+        }
+
+        if (isColliding)
+        {
+            if(rigidBody.velocity.z != 0 && rigidBody.velocity.z > -0.1f) isColliding = false;
+        }
+        else if (!isMoving)
+        {
+            rigidBody.velocity = new Vector3(0, 0, 10f);    
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Collider collider = collision.collider;
+        if (collider.name == "End")
+        {
+            planeGenerator.spawnPlane();
+            collider.gameObject.GetComponent<BoxCollider>().enabled = false;
+            Destroy(collider.transform.parent.gameObject, 1f);
+        }else if (collider.name.StartsWith("Obstacle"))
+        {
+            rigidBody.velocity = Vector3.zero;
+            rigidBody.AddForce(new Vector3(0, 0, -200f));
+            isColliding = true;
         }
     }
 }
