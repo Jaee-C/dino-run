12e306b7452c414596f1f82e70d8903306f1d4e0
Daniel <wengjae@gmail.com>
2022-09-29 15:23:56 +1000

    healthbar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 3.0f;
     [SerializeField] private float speedIncrease = 1.0f;
     [SerializeField] private float dodgeSpeed = 1.0f;
+    public Slider healthBar;
+    public float health = 100.0f;
+    public float maxHealth = 100.0f;
+    public float healthDecay = 0.1f;
 
     private Rigidbody rb;
 
@@ -22,10 +27,15 @@ public class PlayerController : MonoBehaviour
         float xMove = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector3(xMove, rb.velocity.y, speed) * dodgeSpeed;
 
+        health -= healthDecay * Time.deltaTime;
         speed += speedIncrease * Time.deltaTime; // Increases speed over time
+        healthBar.value = health;
     }
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other)
+    {
         Destroy(other.gameObject);
+        health -= 20;
+        healthBar.value = health;
     }
 }
