bb68dbd9c88f441cf5e362c8f5c9a24069da58d2
Daniel <wengjae@gmail.com>
2022-09-29 12:08:10 +1000

    collision detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 1.0f;
-    [SerializeField] private float speedIncrease = 5.0f;
+    [SerializeField] private float speed = 3.0f;
+    [SerializeField] private float speedIncrease = 1.0f;
     [SerializeField] private float dodgeSpeed = 1.0f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        // Move object forward
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        // Move the player
+        float xMove = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector3(xMove, rb.velocity.y, speed) * dodgeSpeed;
 
-        // Player movement
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Translate(Vector3.left * (this.dodgeSpeed * Time.deltaTime));
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Translate(Vector3.right * (this.dodgeSpeed * Time.deltaTime));
-        
-        // Increase speed
-        speed += speedIncrease * Time.deltaTime;
+        speed += speedIncrease * Time.deltaTime; // Increases speed over time
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Destroy(other.gameObject);
     }
 }
