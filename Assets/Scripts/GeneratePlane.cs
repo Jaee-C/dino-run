aa2b26bda70fdbcd054d062a84d52f788666fbdd
Fengax <857514.leofeng@gmail.com>
2022-10-11 02:02:08 +1100

    added obstacle generation algorithm

--- Assets/Scripts/GeneratePlane.cs
+++ Assets/Scripts/GeneratePlane.cs
@@ -18,6 +18,12 @@ public class GeneratePlane : MonoBehaviour
     private GameObject firstPlane;
     public static int PLANE_SIZE = 20;
 
+    struct ObstacleInfo
+    {
+        public Vector3 position;
+        public int radius;
+    }
+
     public GameObject getLastPlane()
     {
         return this.planes.Peek();
@@ -29,15 +35,73 @@ public class GeneratePlane : MonoBehaviour
         Destroy(lastPlane);
     }
 
+    private bool inRadius(Vector3 center, int radius, Vector3 point)
+    {
+        bool xRange = point.x >= center.x - radius && point.x <= center.x + radius;
+        bool zRange = point.z >= center.z - radius * 2 && point.z <= center.z + radius * 2;
+
+        if (xRange && zRange)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
     // Randomly generate obstacles for the plane
     public void generateObstacles()
     {
-        float obstacleWidth = 1.0f;
+        List<ObstacleInfo> obstacleList = new List<ObstacleInfo>();
+
         // Obstacle is generated when the random value is higher than a obstacleChance
-        for (float x = firstPlane.transform.position.x - PLANE_SIZE/2 + obstacleWidth/2; x < firstPlane.transform.position.x + PLANE_SIZE/2 - obstacleWidth/2; x++)
+        for (float x = firstPlane.transform.position.x - PLANE_SIZE/2; x < firstPlane.transform.position.x + PLANE_SIZE/2; x++)
         {
             for (float z = firstPlane.transform.position.z - 5; z < firstPlane.transform.position.z + 5; z++)
             {
+                // Generate a particle
+                if (Random.value > 0.3f)
+                {
+                    bool isValidPos = true;
+                    foreach(ObstacleInfo info in obstacleList)
+                    {
+                        if(inRadius(info.position, info.radius, new Vector3(x, 0, z)))
+                        {
+                            isValidPos = false;
+                            break;
+                        }
+                    }
+
+                    if (isValidPos)
+                    {
+                        float rnd = Random.value;
+                        // Randomly choose size
+                        int size;
+                        if (rnd < 0.33f)
+                        {
+                            size = 1;
+                        }
+                        else if (rnd < 0.66f)
+                        {
+                            size = 2;
+                        }
+                        else
+                        {
+                            size = 3;
+                        }
+
+                        GameObject generatedObstacle = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        generatedObstacle.transform.parent = firstPlane.transform;
+                        generatedObstacle.transform.localScale = new Vector3(size / 2f, 3, size * 2 / 2f);
+                        generatedObstacle.transform.position = new Vector3(x, 0, z);
+                        generatedObstacle.GetComponent<Renderer>().material.color = new Color(1, 0, 0);
+
+                        obstacleList.Add(new ObstacleInfo() { position = new Vector3(x, 0, z), radius = size * 3 });
+                    }
+                }
+
+                /*
                 float height = Random.value * 10f;
                 if (height >= obstacleChance)
                 {
@@ -58,6 +122,7 @@ public class GeneratePlane : MonoBehaviour
                     generatedObstacle.transform.localScale = new Vector3(obstacleWidth, height - 7f, 1);
                     generatedObstacle.transform.position = new Vector3(x, 0 + (height - 7f) * 0.5f, z);
                 }
+                */
             }
         }
     }
