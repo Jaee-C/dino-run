6e0e99dd2b4c7bd35bc3de587858674bf54b14dd
Daniel <wengjae@gmail.com>
2022-10-15 22:51:29 +1100

    fix restart button

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class Restart : MonoBehaviour
 {
     public void RestartGame() {
-        SceneManager.LoadScene("RestartButton");
+        SceneManager.LoadScene("StartScene");
     }
 
     
